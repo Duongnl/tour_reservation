a4be4bdb56931605523f3d8f11cf74c93d525fe5
LocHoang1 <lochoang2101@gmail.com>
2024-10-05 20:49:25 +0700

    fix heder footer

--- src/main/java/com/group21/tour_reservation/controller/client/HomePageController.java
+++ src/main/java/com/group21/tour_reservation/controller/client/HomePageController.java
@@ -5,19 +5,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @Controller
 public class HomePageController {
 
     @GetMapping("/")
     public String getHomePage(Model model) {
-        return "client/layout/home-page.html";
+        return "client/home.html";
     }
-    
+
     @GetMapping("/tour")
     public String getTour(Model model) {
         return "client/tour.html";
     }
-    
 
 }
