b0bdb78648ec53b2e748efd3c5fb1abb25db6277
HuuKhiem <huukhiem1909@gmail.com>
2024-11-26 14:07:06 +0700

    udate

--- src/main/java/com/group21/tour_reservation/controller/client/PersonalInfoController.java
+++ src/main/java/com/group21/tour_reservation/controller/client/PersonalInfoController.java
@@ -12,18 +12,34 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
 @Controller
 public class PersonalInfoController {
     @Autowired
     private CustomerService customerService;
-    @GetMapping("/personal-info")
+        @GetMapping("/personal-info")
     public String index(Model model) {
         model.addAttribute("customer", customerService.getCustomer("2"));
         return "client/customer/index.html";
     }
     @GetMapping("/personal-info/edit/{slug}")
-    public String EditView(Model model,@PathVariable("slug") String slug) {
+    public String EditView(Model model,@PathVariable("slug") String slug) throws ParseException {
+
         Customer customer = customerService.getCustomer(slug);
+//        System.out.println("slug: " + customer.getBirthday());
+//
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+//
+//        LocalDate localDate = LocalDate.parse(customer.getBirthday().toString(), formatter);
+//        customer.setBirthday(localDate);
+
+
+
         if (customer == null) {
             return "admin/404.html";
         }
